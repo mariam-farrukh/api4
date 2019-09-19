3a1e240c65d587a07bfdd98c81f5cc3754b059a9
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-19 12:57:31 -0500

    deploy to heroku using pg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5002;
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(`\n*** Server Running on port: ${port} ***\n`);
 });
